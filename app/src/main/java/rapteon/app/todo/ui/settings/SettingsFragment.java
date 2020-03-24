fbde21c94c01fb5947ba2b90df6b6d7cb4e78372
rapteon <surajnaranatt@gmail.com>
2020-03-24 14:55:52 +0530

    Added switch in fragment_settings.xml for email notification switch.

--- app/src/main/java/rapteon/app/todo/ui/settings/SettingsFragment.java
+++ app/src/main/java/rapteon/app/todo/ui/settings/SettingsFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import rapteon.app.todo.R;
@@ -23,13 +20,7 @@ public class SettingsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
